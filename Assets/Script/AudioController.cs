5fe2e71d7ae7a24ebfeae74ccc30f16e151b77f3
jasxlee <jasmineleehm@gmail.com>
2021-07-11 10:56:05 +0800

    fixed audio to not restart on scene change, toggle audio using pacebar

---
+++ Assets/Script/AudioController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour
+{
+
+    private static AudioController instance = null;
+    public static AudioController Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Assets/Script/AudioToggleController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioToggleController : MonoBehaviour
+{
+    AudioSource audioSource;
+    public GameObject audioToggleButton;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.mute = !audioSource.mute;
+        }
+    }
+}
